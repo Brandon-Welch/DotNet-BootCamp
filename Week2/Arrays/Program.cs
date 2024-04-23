39a217c442528b978933e45d0a4f8fcf096c462c
Brandon-Welch <bwelch3800@hotmail.com>
2024-04-23 07:33:32 -0400

    ading Arrays

---
+++ Week2/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿//Arrays
+
+/*
+-Problem: There is a lot of DATA that exists!!!
+-Managing each piece in its own variable is going to become impractical/borderline impossible.
+-Alot is quite similiar or related, whether its by purpose or even data type
+
+-We need to introduce means to store multiplepieces of data within one device/variable.
+
+-Arrays offer a way for us to very simply store multiple values of the same data type
+
+-Syntax:
+    DataType[] variableName = new DataType[size];
+
+*/
+
+//Basic Example 
+int[] numbers = new int[5]; //this is NOT an integer, it is an Array with Integers
