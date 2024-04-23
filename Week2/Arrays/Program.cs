ae326cfe8ac9c7dd4729ed864c08a01741b198d6
Brandon-Welch <bwelch3800@hotmail.com>
2024-04-23 16:22:56 -0400

    updates

--- Week2/Arrays/Program.cs
+++ Week2/Arrays/Program.cs
@@ -3,16 +3,84 @@
 /*
 -Problem: There is a lot of DATA that exists!!!
 -Managing each piece in its own variable is going to become impractical/borderline impossible.
--Alot is quite similiar or related, whether its by purpose or even data type
+-A lot of this data is quite similiar or related, whether its by purpose or even data type
 
 -We need to introduce means to store multiplepieces of data within one device/variable.
 
 -Arrays offer a way for us to very simply store multiple values of the same data type
+
+-Arrays (in C# and maybe Java) have a fixed size upon instantiation (initalization).
+-Arrays use indexes (just like strings) - an Array of size 5 would have indexes 0-4
 
 -Syntax:
     DataType[] variableName = new DataType[size];
 
 */
 
-//Basic Example 
-int[] numbers = new int[5]; //this is NOT an integer, it is an Array with Integers
+//Basic Example
+using System.Threading.Tasks.Dataflow;
+
+System.Console.WriteLine("-----Basic Example------");
+int[] numbers = new int[5]; //this is NOT an integer, it is an Array that can hold 5 ints
+System.Console.WriteLine(numbers.Length);
+
+//Assign a value to any *element* of this array
+numbers[0] = 10; //assigns value 10 to the 0th index
+numbers[1] = 11;
+numbers[2] = 12;
+
+//Recall/use said value stored inside any *element* of this array
+System.Console.WriteLine("-----Recall Example------");
+System.Console.WriteLine(numbers[0]);
+System.Console.WriteLine(numbers[1]);
+System.Console.WriteLine(numbers[4]);
+//System.Console.WriteLine(numbers[10]); //IndexOutOfRangeException - acceptable indexes for this array are 0-4
+//System.Console.WriteLine(numbers[-1]); //Cannot use negative indices (in C#)
+//System.Console.WriteLine(numbers); //returns "System.Int32[]"
+
+//Foreach Loop
+System.Console.WriteLine("-----Foreach Example------");
+string arrString = "[";
+foreach(int num in numbers)
+{
+    arrString += num + ","; //concatenate
+}
+arrString = arrString.Remove(arrString.Length - 1); //-1 removes the last string which in this example actually removed the last , we didnt need
+arrString += "]";
+System.Console.WriteLine(arrString);
+
+//string result = string.Join (", ", Array.ConvertAll(numbers, x => x.ToString))
+//---------
+System.Console.WriteLine("--another way to do it---");
+string[] numberStrings = new string[numbers.Length];
+for (int i = 0; i < numbers.Length; i++)
+{   
+    numberStrings[i] = numbers[i].ToString();
+}
+System.Console.WriteLine(string.Join(",", numberStrings));
+
+//---------
+System.Console.WriteLine("--simplified but not ideal---");
+foreach (int num in numbers)
+{
+    System.Console.Write(num + " ");
+}
+System.Console.WriteLine(); //simple line break
+
+//Even less Important
+//Multi-dimensional Arrays
+//Syntax:
+int[,] twoDimArray = new int[3,3];
+
+twoDimArray[0,0] = 1;
+twoDimArray[0,1] = 2;
+
+//----------back to reality
+
+//Alternative syntax for instantiating Arrays
+System.Console.WriteLine("--Alt Example---");
+string[] words = ["Hello", "Hi", "Hey"]; //provided 3 values, so the size of the array is interpretted as 3 ****CAN USE =[] OR ={}
+System.Console.WriteLine(words[2]);
+System.Console.WriteLine(words[1]);
+words [1] = "Bye"; //can reassign the value set above
+System.Console.WriteLine(words[1]);
--- Week2/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Week2/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44af8befbd04ae7c77e977f04d0b03dd0ab7c6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc1497ef1f7a0b4a042642964f88ea5aeb89150")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
