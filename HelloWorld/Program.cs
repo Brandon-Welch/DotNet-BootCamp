994ccfa7c4bef667ea82433a797fc1ebca794ab5
Brandon-Welch <bwelch3800@hotmail.com>
2024-04-30 13:17:09 -0400

    organizing

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//ALL programming languages have "comment" tags. Which means you can type whatever you want and it means nothing to the program
+//Single line comments
+
+/*
+Multi-line
+comment option
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello \n");
+Console.WriteLine("Goodbye, World!");
+Console.Write("Some More Text");
+Console.Write("Car Goes Vroom \n"); // \n is an escape character -> represents new line
+Console.Write("add in tab \t"); // \t is an escape character -> represents tab
+Console.WriteLine("\"Hello\" World"); // \" is an escape character -> represents double quote
+Console.WriteLine("Add a backslash\\"); // \\ is an escape character -> represents adding a backslash
+System.Console.WriteLine("Test");
+Console.WriteLine(12345);
+Console.WriteLine(12345+12345);
+
+/*
+Classes/Objects - PascalCasing (first letter of each word capitablized)
+methods - PascalCasing *Example: DoSomething();
+variable - camelCasing (first letter or first word isnt capitalized, remaining first letter of remainiing words capitalized) *Example: numberOfBooks
+*/
