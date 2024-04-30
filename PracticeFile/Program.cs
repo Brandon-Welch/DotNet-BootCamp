786309bef876d16034560562e9f46c741cac2d10
Brandon-Welch <bwelch3800@hotmail.com>
2024-04-30 17:16:14 -0400

    cleanup

--- PracticeFile/Program.cs
+++ PracticeFile/Program.cs
@@ -5,10 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        ReversedString();
-        RyanReverseString();
-        MaxValueInArray();
+        System.Console.WriteLine("\n---Begin---\n"); //define beginning of program and leave a blank line after cmd line and before program
 
+        Thread.Sleep(1000);
+        ReversedString(); //goes backwards indexing the string?
+        Thread.Sleep(1500);
+        RyanReverseString(); //pulls each character starting at the end individually?
+        Thread.Sleep(1500);
+        MaxValueInArray(); //take numbers in array and returns highest (max) value
+
+        System.Console.WriteLine("\n---End---\n"); //define end of program and leave a blank line after program and before cmd line
     }
 
 
@@ -33,7 +39,7 @@ class Program
         System.Console.WriteLine("----------------");
     }
 
-    public static void RyanReverseString() //pulls each character starting at the end individually?
+    public static void RyanReverseString() 
     {
         System.Console.WriteLine("-----String Reversal 2-----");
         System.Console.Write("Enter a string of text: ");
@@ -48,7 +54,7 @@ class Program
         System.Console.WriteLine("----------------");
     }
 
-    public static void ReversedString() //goes backwards indexing the string?
+    public static void ReversedString() 
     {
         System.Console.WriteLine("-----String Reversal 1-----");
         System.Console.Write("Enter a string of text: ");
