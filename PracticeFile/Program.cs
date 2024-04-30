8b1ff24a01eea5436891c805565fab5df0ecf771
Brandon-Welch <bwelch3800@hotmail.com>
2024-04-30 16:49:14 -0400

    add practice file

---
+++ PracticeFile/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Formats.Asn1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReversedString();
+        RyanReverseString();
+        MaxValueInArray();
+
+    }
+
+
+
+
+    //Methods
+    public static void MaxValueInArray()
+    {
+        System.Console.WriteLine("-----Max Value in Array-----");
+        int maxNum = 0;
+        int[] numberArray = { 1, 5, 9, 15, 72, 99, 107, 114, 32, 55, 4 };
+                
+        foreach (int num in numberArray)
+        {
+            if (num > maxNum)
+            {
+                maxNum = num;
+            }
+        }
+
+        System.Console.WriteLine("Max number in your Array is: " + maxNum);
+        System.Console.WriteLine("----------------");
+    }
+
+    public static void RyanReverseString() //pulls each character starting at the end individually?
+    {
+        System.Console.WriteLine("-----String Reversal 2-----");
+        System.Console.Write("Enter a string of text: ");
+        string str = System.Console.ReadLine();
+        string reverse = "";
+        
+        foreach (char c in str)
+        {
+            reverse = c + reverse;
+        }
+        System.Console.WriteLine("String in reverse: " + reverse);
+        System.Console.WriteLine("----------------");
+    }
+
+    public static void ReversedString() //goes backwards indexing the string?
+    {
+        System.Console.WriteLine("-----String Reversal 1-----");
+        System.Console.Write("Enter a string of text: ");
+
+        string inputString = System.Console.ReadLine();
+        string reversedString = "";
+        
+        for (int i = inputString.Length - 1; i >= 0; i--)
+        {
+            reversedString += inputString[i];
+        }
+        
+        System.Console.WriteLine("String in reverse: " + reversedString);
+        System.Console.WriteLine("----------------");
+    }
+
+}
