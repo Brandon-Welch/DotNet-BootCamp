9b4b0288aeefb390edb1f1ad21049711bbcfb8aa
Brandon-Welch <bwelch3800@hotmail.com>
2024-05-19 18:31:11 -0400

    w5 updates

--- Week 5/Databases/MovieApp/Util/UserStorage.cs
+++ Week 5/Databases/MovieApp/Util/UserStorage.cs
@@ -3,12 +3,12 @@ class UserStorage
     public Dictionary<int, User> users;
     public int idCounter = 1; //normall start at 1 and then future is idCounter++
 
-    //Making this constructor give us some pre-loaded Movies to work with.
+    //Making this constructor give us some pre-loaded Accounts to work with.
     public UserStorage()
     {
-        User user1 = new User(idCounter, "BWelch", "pass1", "user"); idCounter++;
-        User user2 = new User(idCounter, "DHolliday", "pass2", "user"); idCounter++;
-        User user3 = new User(idCounter, "KMcCallister", "pass3", "admin"); idCounter++;
+        User user1 = new User(idCounter, "ryan", "pass1", "user"); idCounter++;
+        User user2 = new User(idCounter, "jonathan", "pass2", "user"); idCounter++;
+        User user3 = new User(idCounter, "admin", "pass3", "admin"); idCounter++;
 
         users = [];
         users.Add(user1.Id, user1);
