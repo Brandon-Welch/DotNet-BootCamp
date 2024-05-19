5345ee79b9c6dbbbce4a59452fff9e02f1a8788d
Brandon-Welch <bwelch3800@hotmail.com>
2024-05-19 18:30:41 -0400

    w5 updates

--- Week 3/CS-OOP/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Week 3/CS-OOP/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6bc2a02ffff7908bb7205b606469f8bab12462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991a877b74eb1a8860869914fe7b48724ac112d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
